78bc85d0954706d550906ffe0d7c2a78e5e35eba
h4ckx0r <natanagoor@gmail.com>
2026-02-18 14:27:01 +0100

    feat: migraciones hechas

---
+++ database/migrations/2026_02_18_140604_create_reportes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reportes', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('solicitudId')->constrained('solicitudes')->onDelete('restrict');
+            $table->date('fecha');
+            $table->string('titulo');
+            $table->text('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reportes');
+    }
+};
